292665f784622b42692ec71570f06263cd1a35cb
iyaltamizh <iyaltamizh@gmail.com>
2023-05-21 14:46:30 +0530

    device_address changes sample added, for simple bluetooth printer 1.8

--- app/src/main/java/com/iyaltamizh/apihelp/MainActivity.java
+++ app/src/main/java/com/iyaltamizh/apihelp/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setComponent(new ComponentName("com.iyaltamizh.bluetoothprint", "com.iyaltamizh.bluetoothprint.Print"));
         intent.putExtra("content","Hello World\n\n Content from test app\n");
+
+        //Optional param for direct print without showing the printer select screen --- Replace with your printer mac id
+        intent.putExtra("device_address","0F:02:18:B0:53:AA");
         startActivity(intent);
     }
 }
